303eaa5bb219420209eb50543dfcf03539440b41
MikeGC18 <mrobertgc@gmail.com>
2026-05-11 19:36:55 +0200

    archivos

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { CustomerField } from '@/app/lib/definitions';
+import { CustomerField, State } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
