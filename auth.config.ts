82c4b1645262be4f2e08db8b284759f0810027e1
MikeGC18 <mrobertgc@gmail.com>
2026-05-11 20:46:13 +0200

    archivos

--- auth.config.ts
+++ auth.config.ts
@@ -4,18 +4,19 @@ export const authConfig = {
     pages: {
         signIn: '/login',
     },
+    trustHost: true,
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
+                return false;
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
             return true;
         },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [],
 } satisfies NextAuthConfig;
